a311834cc82c5a17b526212f69811d546687eea6
carnafon <ander.echebarria@gmail.com>
2025-10-03 17:02:24 +0200

    tasilwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ // Asegúrate de que escanea todos los archivos React
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
