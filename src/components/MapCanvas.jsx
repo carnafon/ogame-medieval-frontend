3d2c358fd6a612aa8ae59c8879ed522acc2f50f8
carnafon <ander.echebarria@gmail.com>
2025-10-04 13:03:40 +0200

    logs

--- src/components/MapCanvas.jsx
+++ src/components/MapCanvas.jsx
@@ -33,7 +33,7 @@ export default function MapCanvas({ players = [], activeId, gridSize = 100, cell
         if (!p) return;
         const px = p.x_coord  * cellSize * scale + offset.x_coord + (cellSize * scale) / 2;
         const py = p.y_coord  * cellSize * scale + offset.y_coord  + (cellSize * scale) / 2;
-        //console.log("coorrdenada", px,py,players.id);
+        console.log("coorrdenada", px,py,p.id,p.x_coord,p.y_coord);
         let jugActivo = p.id;
         if(p.is_current_user === false)
           {jugActivo=0;}
